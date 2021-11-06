07ce2c67297f198cc934ae0993b661df618e1249
Dharanivarma9 <91324762+Dharanivarma9@users.noreply.github.com>
2021-11-06 12:25:07 +0530

    Update provider.tf

--- MAINCODE/provider.tf
+++ MAINCODE/provider.tf
@@ -12,9 +12,9 @@ provider "azurerm" {
 
    version = "~>2.0"
 
-   subscription_id="1cb7886c-150e-4693-9699-4629cff99509"
-     tenant_id="2bc247ad-05fc-47c1-9af6-c62465afd6f3"
-     client_id="ec0f64ad-95d3-437f-a761-eb498a73ec05"
+   subscription_id="c5a3c50f-40e0-43ad-8722-e39ac7f364e7"
+     tenant_id="dca0965c-9bd4-425f-8e9c-cf91dbc83a39"
+     client_id="f3d1eb86-1dd6-4d68-80c8-b1c5b31fb973"
   features {
   }
 }
